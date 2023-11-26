652288868fe8c0e0e48774bb12e2543e7b1b28f1
AmanK2004 <amanrkumar2022@gmail.com>
2023-11-25 18:47:28 -0800

    Request Card Component in next.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import './globals.css';
 import KanbanBoard from './components/KanbanBoard';
 import NavBar from './components/NavBar';
+import RequestCard from './components/RequestCard'; 
 
 export default function Home() {
   return (
     <div>
-      <NavBar/>
+      <RequestCard/>
     </div>
     
   )
