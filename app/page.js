b0667f1096ab3eb76437fd66399cc5e3f029e807
AmanK2004 <amanrkumar2022@gmail.com>
2023-11-25 18:36:02 -0800

    NavBar component in next.js

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import './globals.css';
 import KanbanBoard from './components/KanbanBoard';
+import NavBar from './components/NavBar';
 
 export default function Home() {
   return (
     <div>
-      <KanbanBoard/>
+      <NavBar/>
     </div>
     
   )
