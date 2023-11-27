5154c51a78395a97d59b39231873a011f3322668
Mantej Lamba <mlamba@usc.edu>
2023-11-26 18:24:16 -0800

    adding navabr to layout file

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
-import Link from 'next/link';
-import '../app/globals.css';
+import Link from "next/link";
+import "../app/globals.css";
+import NavBar from "./components/NavBar";
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
         <main>
+          <NavBar />
           {children}
         </main>
       </body>
