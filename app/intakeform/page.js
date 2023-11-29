0d4811c836c8f20ceda55a2ea22e46b06be6826a
Mantej Lamba <mlamba@usc.edu>
2023-11-29 14:39:14 -0800

    styles

--- app/intakeform/page.js
+++ app/intakeform/page.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 
 import {
   Button,
-  Card, 
+  Card,
   Checkbox,
   Label,
   TextInput,
@@ -57,7 +57,7 @@ export default function IntakeForm() {
     const theNewKey = pushRef.key;
 
     setNewKey(theNewKey);
-    router.push("/signup");
+    router.push("/thankyou");
   };
 
   const handleInputChange = (id, value) => {
@@ -103,237 +103,239 @@ export default function IntakeForm() {
   };
 
   return (
-    <div style={{ background: '#FAFAFA' }} className="flex flex-col items-center justify-center min-h-screen overflow-y-auto">
-    <h1 className="text-3xl font-bold tracking-tight text-#C3500F mb-4 text-center mt-8">
-    Elissa Martial's Character Design Form
-    </h1>
+    <div
+      style={{ background: "#FAFAFA" }}
+      className="flex flex-col items-center justify-center min-h-screen overflow-y-auto"
+    >
+      <h1 className="text-3xl font-bold tracking-tight text-#C3500F mb-4 text-center mt-8">
+        Elissa Martial's Character Design Form
+      </h1>
       <form
         className="max-w-md mx-auto flex flex-col gap-4"
         onSubmit={handleSubmit}
       >
         <Card className="max-w-md p-4 bg-gray-100 shadow-md rounded-md">
-        <div className="basic-information-card">
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="firstName" value="First Name" />
+          <div className="basic-information-card">
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="firstName" value="First Name" />
+              </div>
+              <TextInput
+                id="firstName"
+                type="text"
+                placeholder="John"
+                required
+                onChange={(e) => handleInputChange("firstName", e.target.value)}
+              />
             </div>
-            <TextInput
-              id="firstName"
-              type="text"
-              placeholder="John"
-              required
-              onChange={(e) => handleInputChange("firstName", e.target.value)}
-            />
+
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="lastName" value="Last Name" />
+              </div>
+              <TextInput
+                id="lastName"
+                type="text"
+                placeholder="Doe"
+                required
+                onChange={(e) => handleInputChange("lastName", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="title" value="Title of Project" />
+              </div>
+              <TextInput
+                id="title"
+                type="text"
+                placeholder="My Project"
+                required
+                onChange={(e) => handleInputChange("title", e.target.value)}
+              />
+            </div>
           </div>
 
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="lastName" value="Last Name" />
+              <Label htmlFor="email1" value="Your email" />
             </div>
             <TextInput
-              id="lastName"
-              type="text"
-              placeholder="Doe"
+              id="email1"
+              type="email"
+              placeholder="name@flowbite.com"
               required
-              onChange={(e) => handleInputChange("lastName", e.target.value)}
+              onChange={(e) => handleInputChange("email", e.target.value)}
             />
           </div>
-
-          <div>
-            <div className="mb-2 block">
-              <Label htmlFor="title" value="Title of Project" />
-            </div>
-            <TextInput
-              id="title"
-              type="text"
-              placeholder="My Project"
-              required
-              onChange={(e) => handleInputChange("title", e.target.value)}
-            />
-          </div>
-        </div>
-
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="email1" value="Your email" />
-          </div>
-          <TextInput
-            id="email1"
-            type="email"
-            placeholder="name@flowbite.com"
-            required
-            onChange={(e) => handleInputChange("email", e.target.value)}
-          />
-        </div>
         </Card>
 
-
         <Card className="max-w-md p-4 bg-gray-100 shadow-md rounded-md">
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="characterFraming" value="Character Framing" />
-          </div>
-          <Select
-            id="characterFraming"
-            required
-            onChange={(e) =>
-              handleInputChange("characterFraming", e.target.value)
-            }
-          >
-            <option value="" disabled selected>
-              Select an option
-            </option>
-            <option value="headshot">Headshot</option>
-            <option value="half body">Half Body</option>
-            <option value="full body">Full Body</option>
-          </Select>
-        </div>
-
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="backgroundType" value="Background Type" />
-          </div>
-          <Select
-            id="backgroundType"
-            required
-            onChange={(e) =>
-              handleInputChange("backgroundType", e.target.value)
-            }
-          >
-            <option value="" disable selected>
-              Select an option
-            </option>
-            <option value="complex">Complex</option>
-            <option value="simple">Simple</option>
-            <option value="single color">Single Color</option>
-          </Select>
-        </div>
-
-        <fieldset className="flex max-w-md flex-col gap-4">
-          <legend className="mb-4">Number of Characters</legend>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="numberOfCharacters"
-              name="numberOfCharacters"
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="characterFraming" value="Character Framing" />
+            </div>
+            <Select
+              id="characterFraming"
+              required
               onChange={(e) =>
-                handleInputChange("numberOfCharacters", e.target.value)
+                handleInputChange("characterFraming", e.target.value)
               }
-            />
-            <Label htmlFor="0">0</Label>
+            >
+              <option value="" disabled selected>
+                Select an option
+              </option>
+              <option value="headshot">Headshot</option>
+              <option value="half body">Half Body</option>
+              <option value="full body">Full Body</option>
+            </Select>
           </div>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="numberOfCharacters"
-              name="numberOfCharacters"
-              value="1"
+
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="backgroundType" value="Background Type" />
+            </div>
+            <Select
+              id="backgroundType"
+              required
               onChange={(e) =>
-                handleInputChange("numberOfCharacters", e.target.value)
+                handleInputChange("backgroundType", e.target.value)
               }
-            />
-            <Label htmlFor="1">1</Label>
+            >
+              <option value="" disable selected>
+                Select an option
+              </option>
+              <option value="complex">Complex</option>
+              <option value="simple">Simple</option>
+              <option value="single color">Single Color</option>
+            </Select>
           </div>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="numberOfCharacters"
-              name="numberOfCharacters"
-              value="2"
-              onChange={(e) =>
-                handleInputChange("numberOfCharacters", e.target.value)
-              }
-            />
-            <Label htmlFor="2">2</Label>
-          </div>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="numberOfCharacters"
-              name="numberOfCharacters"
-              value="3+"
-              onChange={(e) =>
-                handleInputChange("numberOfCharacters", e.target.value)
-              }
-            />
-            <Label htmlFor="3+">3+</Label>
-          </div>
-        </fieldset>
 
-        <fieldset className="flex max-w-md flex-col gap-4">
-          <legend className="mb-4">Budget</legend>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="budget"
-              name="budget"
-              value="$0-$100"
-              onChange={(e) => handleInputChange("budget", e.target.value)}
-            />
-            <Label htmlFor="$0-$100">$0-$100</Label>
-          </div>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="$100-$250"
-              name="budget"
-              value="$100-$250"
-              onChange={(e) => handleInputChange("budget", e.target.value)}
-            />
-            <Label htmlFor="$100-$250">$100-$250</Label>
-          </div>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="$250-$500"
-              name="budget"
-              value="$250-$500"
-              onChange={(e) => handleInputChange("budget", e.target.value)}
-            />
-            <Label htmlFor="$250-$500">$250-$500</Label>
-          </div>
-          <div className="flex items-center gap-2">
-            <Radio
-              id="$500+"
-              name="budget"
-              value="$500+"
-              onChange={(e) => handleInputChange("budget", e.target.value)}
-            />
-            <Label htmlFor="$500+">$500+</Label>
-          </div>
-        </fieldset>
+          <fieldset className="flex max-w-md flex-col gap-4">
+            <legend className="mb-4">Number of Characters</legend>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="numberOfCharacters"
+                name="numberOfCharacters"
+                onChange={(e) =>
+                  handleInputChange("numberOfCharacters", e.target.value)
+                }
+              />
+              <Label htmlFor="0">0</Label>
+            </div>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="numberOfCharacters"
+                name="numberOfCharacters"
+                value="1"
+                onChange={(e) =>
+                  handleInputChange("numberOfCharacters", e.target.value)
+                }
+              />
+              <Label htmlFor="1">1</Label>
+            </div>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="numberOfCharacters"
+                name="numberOfCharacters"
+                value="2"
+                onChange={(e) =>
+                  handleInputChange("numberOfCharacters", e.target.value)
+                }
+              />
+              <Label htmlFor="2">2</Label>
+            </div>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="numberOfCharacters"
+                name="numberOfCharacters"
+                value="3+"
+                onChange={(e) =>
+                  handleInputChange("numberOfCharacters", e.target.value)
+                }
+              />
+              <Label htmlFor="3+">3+</Label>
+            </div>
+          </fieldset>
 
-        <div>
-          <div className="mb-2 block">
-            <Label htmlFor="deadline" value="Estimated Deadline" />
-          </div>
-          <TextInput
-            id="deadline"
-            type="date"
-            required
-            onChange={(e) => handleInputChange("deadline", e.target.value)}
-          />
-        </div>
+          <fieldset className="flex max-w-md flex-col gap-4">
+            <legend className="mb-4">Budget</legend>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="budget"
+                name="budget"
+                value="$0-$100"
+                onChange={(e) => handleInputChange("budget", e.target.value)}
+              />
+              <Label htmlFor="$0-$100">$0-$100</Label>
+            </div>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="$100-$250"
+                name="budget"
+                value="$100-$250"
+                onChange={(e) => handleInputChange("budget", e.target.value)}
+              />
+              <Label htmlFor="$100-$250">$100-$250</Label>
+            </div>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="$250-$500"
+                name="budget"
+                value="$250-$500"
+                onChange={(e) => handleInputChange("budget", e.target.value)}
+              />
+              <Label htmlFor="$250-$500">$250-$500</Label>
+            </div>
+            <div className="flex items-center gap-2">
+              <Radio
+                id="$500+"
+                name="budget"
+                value="$500+"
+                onChange={(e) => handleInputChange("budget", e.target.value)}
+              />
+              <Label htmlFor="$500+">$500+</Label>
+            </div>
+          </fieldset>
 
-        <div>
-          <div className="mb-2 block">
-            <Label
-              htmlFor="large"
-              value="Describe your project in as much detail as possible."
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="deadline" value="Estimated Deadline" />
+            </div>
+            <TextInput
+              id="deadline"
+              type="date"
+              required
+              onChange={(e) => handleInputChange("deadline", e.target.value)}
             />
           </div>
-          <TextInput
-            id="details"
-            type="text"
-            sizing="lg"
-            onChange={(e) => handleInputChange("details", e.target.value)}
-          />
-        </div>
 
-        <div id="fileUpload" className="max-w-md orange">
-          <div className="mb-2 block">
-            <Label htmlFor="file" value="Upload file" />
+          <div>
+            <div className="mb-2 block">
+              <Label
+                htmlFor="large"
+                value="Describe your project in as much detail as possible."
+              />
+            </div>
+            <TextInput
+              id="details"
+              type="text"
+              sizing="lg"
+              onChange={(e) => handleInputChange("details", e.target.value)}
+            />
           </div>
-          <FileInput
-            id="file"
-            multiple
-            helperText="Upload at least 3 reference images per character and 3 for the background (if applicable)"
-            onChange={(e) => handleFileUpload(e.target.files)}
-          />
-        </div>
+
+          <div id="fileUpload" className="max-w-md orange">
+            <div className="mb-2 block">
+              <Label htmlFor="file" value="Upload file" />
+            </div>
+            <FileInput
+              id="file"
+              multiple
+              helperText="Upload at least 3 reference images per character and 3 for the background (if applicable)"
+              onChange={(e) => handleFileUpload(e.target.files)}
+            />
+          </div>
         </Card>
 
         <Button
--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-// ... (your existing imports)
 import React, { useState } from "react";
 import { Button, Card } from "flowbite-react";
 import NavBar from "../components/NavBar";
@@ -10,16 +9,35 @@ import Logo from "../components/Logo";
 
 export default function Profile() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center h-screen">
       <div className="text-center">
-        <Logo size="small" />
+        <div
+          className="mt-4 flex justify-center rounded-full overflow-hidden w-24 h-24"
+          styles={{ paddingLeft: "20%" }}
+        >
+          <Logo size="small" />
+        </div>
 
         <div className="mt-8">
           <h1 className="text-2xl font-bold">
-            Thank you for submitting the form!
+            Success! You've requested a project from Elissa.
           </h1>
-          <h1 className="text-2xl font-bold">
-            You will hear back shortly from Elissa Martial!
+          <h1
+            className="text-lg mt-10 w-80 ml-10"
+            style={{ paddingLeft: "17%" }}
+          >
+            A confirmation email containing your request details has been
+            forwarded to you. You will be notified via email if your request is
+            accepted, denied, or if further discussion is needed!
+          </h1>
+          <h1 className="text-lg mt-10" style={{ color: "#C3500F" }}>
+            Thanks for using Cattail.{" "}
+            <a
+              href="https://google.com"
+              style={{ color: "#C3500F", textDecoration: "underline" }}
+            >
+              Learn More
+            </a>
           </h1>
         </div>
       </div>
