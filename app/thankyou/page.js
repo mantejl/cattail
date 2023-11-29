5976cc20c332a96f1430384b9bfd1899231e6125
AmanK2004 <amanrkumar2022@gmail.com>
2023-11-29 14:48:15 -0800

    thank you styling

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -9,37 +9,27 @@ import Logo from "../components/Logo";
 
 export default function Profile() {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <div className="text-center">
-        <div
-          className="mt-4 flex justify-center rounded-full overflow-hidden w-24 h-24"
-          styles={{ paddingLeft: "20%" }}
-        >
-          <Logo size="small" />
-        </div>
+    <div className="flex flex-col justify-center h-screen text-center">
+      <div className="mt-4 mx-auto rounded-full overflow-hidden w-32 h-34" style={{ borderRadius: "50%" }}>
+        <Logo size="small" />
+      </div>
 
-        <div className="mt-8">
-          <h1 className="text-2xl font-bold">
-            Success! You've requested a project from Elissa.
-          </h1>
-          <h1
-            className="text-lg mt-10 w-80 ml-10"
-            style={{ paddingLeft: "17%" }}
+      <div className="mt-8">
+        <h1 className="text-2xl font-bold">
+          Success! You've requested a project from Elissa.
+        </h1>
+        <h1 className="text-lg mt-10 max-w-lg mx-auto">
+          A confirmation email containing your request details has been forwarded to you. You will be notified via email if your request is accepted, denied, or if further discussion is needed!
+        </h1>
+        <h1 className="text-lg mt-10" style={{ color: "#C3500F" }}>
+          Thanks for using Cattail.{" "}
+          <a
+            href="https://google.com"
+            style={{ color: "#C3500F", textDecoration: "underline" }}
           >
-            A confirmation email containing your request details has been
-            forwarded to you. You will be notified via email if your request is
-            accepted, denied, or if further discussion is needed!
-          </h1>
-          <h1 className="text-lg mt-10" style={{ color: "#C3500F" }}>
-            Thanks for using Cattail.{" "}
-            <a
-              href="https://google.com"
-              style={{ color: "#C3500F", textDecoration: "underline" }}
-            >
-              Learn More
-            </a>
-          </h1>
-        </div>
+            Learn More
+          </a>
+        </h1>
       </div>
     </div>
   );
